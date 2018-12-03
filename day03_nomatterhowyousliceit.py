67385fe03a4306e1c58bbf5ab20825f6b1339bbb
jens <jp@jens.tech>
2018-12-03 09:39:10 +0100

    day03 final

--- day03_nomatterhowyousliceit.py
+++ day03_nomatterhowyousliceit.py
@@ -8,30 +8,36 @@ from typing import Tuple
 #################
 
 
-def claim_to_slice(claim: str) -> Tuple:
-    id_, _, xy, wh = claim.strip().split()
-    x, y = xy.split(',')
-    x = int(x)
-    y = int(y[:-1])
-    w, h = [int(s) for s in wh.split('x')]
-    id_ = int(id_[1:])
-    return id_, x, y, w, h
 
+def parse_claims(claims: [str]) -> [Tuple]:
+    """
+    Parse claims from string to list of tuples of claim properties.
+    """
+    parsed_claims = []
+    for claim in claims:
+        id_, _, xy, wh = claim.strip().split()
+        x, y = xy.split(',')
+        x = int(x)
+        y = int(y[:-1])
+        w, h = [int(s) for s in wh.split('x')]
+        id_ = int(id_[1:])
+        parsed_claims.append((id_, x, y, w, h))
+    return parsed_claims
 
 def overlapping_claims(claims: [str]) -> int:
     fabric = np.zeros((1000, 1000))
 
-    for claim in claims:
-        _, x, y, w, h = claim_to_slice(claim)
+    for claim in parse_claims(claims):
+        _, x, y, w, h = claim
         fabric[x:x+w, y:y+h] += 1
 
-    print("Max claims on one sq = " + str(fabric.max()))
-
-    c = 0
-    for row in fabric:
-        for sqi in row:
-            if sqi > 1:
-                c += 1
+    #c = 0
+    #for row in fabric:
+    #    for sqi in row:
+    #        if sqi > 1:
+    #            c += 1
+    c = np.sum(fabric > 1)
+    
 
     return c
 
@@ -56,12 +62,12 @@ def part1(myinput: str) -> None:
 def intact_claims(claims: [str]) -> [int]:
     intact = []
     fabric = np.zeros((1000, 1000))
-    for claim in claims:
-        i, x, y, w, h = claim_to_slice(claim)
+    for claim in parse_claims(claims):
+        i, x, y, w, h = claim
         fabric[x:x+w, y:y+h] += 1
 
-    for claim in claims:
-        i, x, y, w, h  = claim_to_slice(claim)
+    for claim in parse_claims(claims):
+        i, x, y, w, h  = claim
         if np.all(fabric[x:x+w, y:y+h] == 1):
             intact.append(i)
     return intact
@@ -71,7 +77,6 @@ def part2test() -> None:
     claims = ['#1 @ 1,3: 4x4',
               '#2 @ 3,1: 4x4',
               '#3 @ 5,5: 2x2']
-    print("testresult " + str(intact_claims(claims)))
     assert intact_claims(claims) == [3]
     print("tests passed")
 
